fcc38b11e51a263ae0bd190da35430630de50f3d
Łukasz Chruściel <lchrusciel@gmail.com>
2018-04-11 16:54:25 +0200

    Working gift wrapping

---
+++ spec/OrderProcessor/GiftWrapperOrderProcessorSpec.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace spec\Acme\SyliusExamplePlugin\OrderProcessor;
+
+use Acme\SyliusExamplePlugin\OrderProcessor\GiftWrapperOrderProcessor;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Component\Core\Model\AdjustmentInterface;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Order\Factory\AdjustmentFactoryInterface;
+use Sylius\Component\Order\Processor\OrderProcessorInterface;
+
+class GiftWrapperOrderProcessorSpec extends ObjectBehavior
+{
+    function let(AdjustmentFactoryInterface $adjustmentFactory)
+    {
+        $this->beConstructedWith($adjustmentFactory);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(GiftWrapperOrderProcessor::class);
+    }
+
+    function it_should_implement_the_order_processor_interface()
+    {
+        $this->shouldImplement(OrderProcessorInterface::class);
+    }
+
+    function it_should_add_to_the_order_total_when_the_order_is_gift_wrapped(
+        OrderInterface $order,
+        AdjustmentInterface $adjustment,
+        AdjustmentFactoryInterface $adjustmentFactory
+    ) {
+        $adjustmentFactory->createNew()->willReturn($adjustment);
+        $order->addAdjustment($adjustment)->shouldBeCalled();
+        $this->process($order);
+
+        $adjustment->setAmount(1000)->shouldBeCalled();
+        $adjustment->setType('gift_wrapped')->shouldBeCalled();
+    }
+}
---
+++ src/OrderProcessor/GiftWrapperOrderProcessor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Acme\SyliusExamplePlugin\OrderProcessor;
+
+use Sylius\Component\Core\Model\AdjustmentInterface;
+use Sylius\Component\Order\Factory\AdjustmentFactory;
+use Sylius\Component\Order\Factory\AdjustmentFactoryInterface;
+use Sylius\Component\Order\Model\OrderInterface;
+use Sylius\Component\Order\Processor\OrderProcessorInterface;
+
+class GiftWrapperOrderProcessor implements OrderProcessorInterface
+{
+    /**
+     * @var AdjustmentFactoryInterface
+     */
+    private $adjustmentFactory;
+
+    public function __construct(AdjustmentFactoryInterface $adjustmentFactory)
+    {
+        $this->adjustmentFactory = $adjustmentFactory;
+    }
+
+    /**
+     * @param OrderInterface $order
+     */
+    public function process(OrderInterface $order): void
+    {
+        /** @var AdjustmentInterface $adjustment */
+        $adjustment = $this->adjustmentFactory->createNew();
+
+        $adjustment->setAmount(1000);
+        $adjustment->setType('gift_wrapped');
+
+        $order->addAdjustment($adjustment);
+
+    }
+}
